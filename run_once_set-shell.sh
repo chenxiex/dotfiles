5613546c5b89b7a8afdd84cb62871e5c6a7b2bf2
Anlor <chenxiex@outlook.com>
2025-10-23 00:06:08 +0800

    Set default shell to zsh

--- run_once_set-shell.sh
+++ run_once_set-shell.sh
@@ -2,8 +2,5 @@
 set -e
 
 if command -v zsh >/dev/null 2>&1; then
-    if [ "$SHELL" != "$zsh_path" ]; then
-        echo "Setting default shell to: $zsh_path"
-        usermod -s "$(command -v zsh)" "$USER"
-	fi
+	sudo usermod -s "$(command -v zsh)" "$USER"
 fi
