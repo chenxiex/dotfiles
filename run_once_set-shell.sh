11b93db6d9487f73b4c6b097d1087cbb5c1d918d
Anlor <chenxiex@outlook.com>
2025-10-23 00:01:02 +0800

    Set default shell to zsh

--- run_once_set-shell.sh
+++ run_once_set-shell.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -e
 
-# 检查 zsh 是否在 PATH 中
 if command -v zsh >/dev/null 2>&1; then
-    zsh_path="$(command -v zsh)"
-
-    # 如果当前默认 shell 不是 zsh，则修改
     if [ "$SHELL" != "$zsh_path" ]; then
         echo "Setting default shell to: $zsh_path"
-        chsh -s "$zsh_path"
+        usermod -s "$(command -v zsh)" "$USER"
 	fi
 fi
