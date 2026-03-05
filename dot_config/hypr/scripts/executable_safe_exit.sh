47f0548ec56117c708932c8f73732da404e445f5
Anlor <chenxiex@outlook.com>
2026-03-05 18:14:43 +0800

    Update .config/hypr/scripts/safe_exit.sh

--- dot_config/hypr/scripts/executable_safe_exit.sh
+++ dot_config/hypr/scripts/executable_safe_exit.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
 flatpak kill com.heroicgameslauncher.hgl
+hyprctl keyword monitor DP-5,disable
+sleep 10
 command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit
