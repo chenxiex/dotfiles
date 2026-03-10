dca7b8685f5bc3ba64da4dd3b2878e7ab0eb6683
Anlor <chenxiex@outlook.com>
2026-03-10 09:47:29 +0800

    Add .local/bin/hypr-game.sh

---
+++ dot_local/bin/hypr-game.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+egpu_path="/dev/dri/nvidia-egpu"
+
+shopt -s nullglob
+dp_paths=( /sys/class/drm/*-DP-5 )
+shopt -u nullglob
+
+if [[ -e "$egpu_path" && ${#dp_paths[@]} -gt 0 ]]; then
+    toggle-gpu-display.sh nvidia detect && \
+        start-hyprland
+    toggle-gpu-display.sh nvidia off && \
+        pause && \
+        chvt 2 && \
+        exit
+else
+    echo "Warning: egpu or display not found!"
+    pause && \
+        chvt 2 && \
+        exit
+fi
