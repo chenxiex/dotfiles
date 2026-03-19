13cd8ce129f6b52ce1fb3c1cf54c98588195584f
Anlor <chenxiex@outlook.com>
2026-03-20 00:34:13 +0800

    Remove .config/ibus/rime/default.custom.yaml Remove .config/ibus/rime/ibus_rime.custom.yaml Remove .config/ibus/rime/installation.yaml Remove .config/ibus/rime/rime_mint.custom.yaml Update apply_rime.sh

--- run_after_apply_rime.sh
+++ run_after_apply_rime.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/bash
 cd ~/.config/ibus/rime
-cp -r oh-my-rime/* .
+rsync -a --delete \
+	--exclude='user.yaml' \
+	--exclude='installation.yaml' \
+	--exclude='build' \
+	--exclude='*.userdb' \
+	--exclude='*.custom.yaml' \
+	--exclude='*.gram' \
+	oh-my-rime/ .
+	
