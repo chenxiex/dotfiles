63ed7ab9af651e001da55a27b4eae5520251bd41
Anlor <chenxiex@outlook.com>
2026-03-27 11:58:40 +0800

    Update .config/sh_shared/install.sh

--- dot_config/sh_shared/install.sh
+++ dot_config/sh_shared/install.sh
@@ -1,13 +1,15 @@
 ### aliases
 ALIAS_PATH="$SH_SHARED_DIR/alias"
-if [[ -e "$ALIAS_PATH" ]]; then
+if [[ -e "$ALIAS_PATH" && -z $_INSTALLED_ALIAS ]]; then
+	export _INSTALLED_ALIAS=1
 	source "$ALIAS_PATH"
 fi
 ### End of aliases
 
 ### functions
 FUNC_PATH="$SH_SHARED_DIR/func"
-if [[ -e "$FUNC_PATH" ]]; then
+if [[ -e "$FUNC_PATH" && -z $_INSTALLED_FUNC ]]; then
+	export _INSTALLED_FUNC=1
 	source "$FUNC_PATH"
 fi
 ### End of functions
