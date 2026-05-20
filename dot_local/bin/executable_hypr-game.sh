e66589228a4827678e37a989857f0cfe504fbd2c
Anlor <chenxiex@outlook.com>
2026-05-20 11:29:25 +0800

    Update .local/bin/hypr-game.sh

--- dot_local/bin/executable_hypr-game.sh
+++ dot_local/bin/executable_hypr-game.sh
@@ -2,6 +2,30 @@
 
 egpu_path="/dev/dri/nvidia-egpu"
 
+choose_fallback() {
+    echo "Warning: egpu or display not found!"
+    echo "1) Continue and start Hyprland"
+    echo "2) Switch to tty2"
+
+    while true; do
+        read -r -p "Choose an option [1/2]: " choice
+        case "$choice" in
+            1)
+                start-hyprland
+                chvt 2 && \
+                    exit
+                ;;
+            2)
+                chvt 2 && \
+                    exit
+                ;;
+            *)
+                echo "Invalid option. Please choose 1 or 2."
+                ;;
+        esac
+    done
+}
+
 shopt -s nullglob
 dp_paths=( /sys/class/drm/*-DP-5 )
 shopt -u nullglob
@@ -14,8 +38,5 @@ if [[ -e "$egpu_path" && ${#dp_paths[@]} -gt 0 ]]; then
         chvt 2 && \
         exit
 else
-    echo "Warning: egpu or display not found!"
-    pause.sh && \
-        chvt 2 && \
-        exit
+    choose_fallback
 fi
