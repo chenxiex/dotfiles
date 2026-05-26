deda4066708e082a1716cb58700d07edd9ce6e0d
Anlor <chenxiex@outlook.com>
2026-05-26 10:34:43 +0800

    Update .config/hypr/modules/env-var.lua

--- dot_config/hypr/modules/env-var.lua
+++ dot_config/hypr/modules/env-var.lua
@@ -7,20 +7,27 @@
 local display = require("modules.vars.display")
 local gamemode = require("modules.vars.gamemode")
 
+--- hidpi
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
-
 hl.env("GDK_SCALE", display.scale)
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+hl.env("QT_ENABLE_HIGHDPI_SCALING", "1")
 
+--- For games
 if gamemode then
     hl.env("AQ_DRM_DEVICES", "/dev/dri/nvidia-egpu:/dev/dri/intel-igpu")
 else
     hl.env("AQ_DRM_DEVICES", "/dev/dri/intel-igpu")
 end
 
+-- For wine wayland
+hl.env("WAYLANDDRV_PRIMARY_MONITOR", display.external)
+
+--- Input method
 hl.env("XMODIFIERS", "@im=fcitx")
 hl.env("QT_IM_MODULE", "fcitx")
 hl.env("QT_IM_MODULES", "wayland;fcitx")
 
--- For wine wayland
-hl.env("WAYLANDDRV_PRIMARY_MONITOR", display.external)
+--- GSK_RENDERER
+hl.env("GSK_RENDERER", "opengl")
