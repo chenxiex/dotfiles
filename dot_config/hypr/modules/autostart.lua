2cf58eedf38cc3bd88f19424cb389ab0cebf4cd2
Anlor <chenxiex@outlook.com>
2026-05-28 21:03:51 +0800

    Update .config/hypr/modules/autostart.lua

--- dot_config/hypr/modules/autostart.lua
+++ dot_config/hypr/modules/autostart.lua
@@ -16,12 +16,7 @@ local gamemode = require("modules.vars.gamemode")
 hl.on("hyprland.start", function () 
   hl.exec_cmd("waybar")
   hl.exec_cmd("hyprpaper")
-  hl.exec_cmd("copyq --start-server")
-  hl.exec_cmd("/usr/bin/fcitx5")
   hl.exec_cmd("systemctl --user start hyprland-session.target")
-
-  hl.exec_cmd('gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"')
-  hl.exec_cmd('gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"')
 
   if gamemode then
     hl.exec_cmd("com.heroicgameslauncher.hgl")
