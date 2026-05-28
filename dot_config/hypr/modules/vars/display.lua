fe92d62be0a4259d08c40289dbbaf015ac5b88ab
Anlor <chenxiex@outlook.com>
2026-05-28 21:31:40 +0800

    Update .config/hypr/modules/vars/display.lua

--- dot_config/hypr/modules/vars/display.lua
+++ dot_config/hypr/modules/vars/display.lua
@@ -2,7 +2,7 @@ local gamemode = require("modules.vars.gamemode")
 local display
 
 display = {
-    external = gamemode and "DP-5" or "DP-3",
+    external = gamemode and "DP-5" or "",
     internal = "eDP-1",
     scale = 1.6,
 }
