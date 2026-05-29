0584328ce650d6c43c090fe204a741b6868a0f1e
Anlor <chenxiex@outlook.com>
2026-05-29 18:53:23 +0800

    Remove .config/hypr/modules/misc.lua

--- dot_config/hypr/modules/misc.lua
+++
@@ -1,9 +0,0 @@
-----------------
-----  MISC  ----
-----------------
-
-hl.config({
-    misc = {
-        disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
-    },
-})
