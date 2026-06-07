013fa281e74f9e04a4841a2300669487e2261142
Anlor <chenxiex@outlook.com>
2026-06-07 20:41:32 +0800

    Update .config/hypr/modules/env-var.lua

--- dot_config/hypr/modules/env-var.lua
+++ dot_config/hypr/modules/env-var.lua
@@ -25,6 +25,7 @@ hl.env("QT_QPA_PLATFORMTHEME_QT6", "gtk3")
 --- For games
 if gamemode then
     hl.env("AQ_DRM_DEVICES", "/dev/dri/nvidia-egpu:/dev/dri/intel-igpu")
+    hl.env("LIBVA_DRIVER_NAME", "nvidia")
     hl.env("WAYLANDDRV_PRIMARY_MONITOR", "DP-5")
 else
     hl.env("AQ_DRM_DEVICES", "/dev/dri/intel-igpu")
