f7ff0815380e70eaa05e130c3c1425bf25285dbe
Anlor <chenxiex@outlook.com>
2026-06-12 16:03:32 +0800

    Remove .config/hypr/modules/vars/display.lua

--- dot_config/hypr/modules/vars/display.lua
+++
@@ -1,9 +0,0 @@
-local gamemode = require("modules.vars.gamemode")
-local display
-
-display = {
-    external = gamemode and "DP-5" or "",
-    internal = "eDP-1",
-    scale = 1.5,
-}
-return display
