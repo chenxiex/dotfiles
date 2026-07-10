1f9c56bfa1e647b5c439e237a41a252860ebcdbb
Anlor <chenxiex@outlook.com>
2026-07-11 01:11:31 +0800

    Update .config/hypr/hyprland.lua Update .config/hypr/modules/autostart.lua

--- dot_config/hypr/modules/autostart.lua
+++ dot_config/hypr/modules/autostart.lua
@@ -19,11 +19,9 @@ local systemd_session_targets = hyprland_session_target .. " " .. xdg_autostart_
 local devices_func = require("modules.devices")
 
 hl.on("hyprland.start", function ()
-  hl.exec_cmd("dbus-update-activation-environment --systemd --all")
   hl.exec_cmd("xrdb -merge ~/.config/X11/Xresources")
   hl.exec_cmd("wl-clip-persist --clipboard regular")
   hl.exec_cmd("wvkbd-mobintl -L 250 --hidden")
-  hl.exec_cmd("systemctl --user start " .. hyprland_session_target)
 
   if gamemode then
     hl.exec_cmd("com.heroicgameslauncher.hgl")
